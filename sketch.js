3717230b89be1b2a348cddbe7ebe5c332140b02a
Ravi <raviodpallivar@gmail.com>
2021-01-05 17:17:09 +0530

    added sling

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
+var log6,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,6 +18,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    conlog = new Log(230,180,80,PI/2);
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,7 +39,11 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    
+    log6 = new Log(230,180,80,PI/2);
 
+    chain = new Chain(bird.body,log6.body);
+ 
 }
 
 function draw(){
@@ -61,4 +69,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    conlog.display();
+    log6.display();
+    chain.display();
+
 }
